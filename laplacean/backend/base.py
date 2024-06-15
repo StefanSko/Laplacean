d01f976dee09748e58798714a89b200dce2bcb13
StefanSko <Stefan.sko@gmail.com>
2024-06-15 14:08:20 +0200

    Add HMC base class and methods

---
+++ laplacean/backend/base.py
@@ -0,0 +1,16 @@
+from typing import Protocol, Callable, Union
+from jax.numpy import ndarray
+import numpy as np
+import jax.numpy as jnp
+
+ArrayLike = Union[np.ndarray, jnp.ndarray]
+
+class HMC(Protocol):
+
+    def hmc(self, U: Callable[[ArrayLike], float], grad_U: Callable[[ArrayLike], ArrayLike], epsilon: float, L: int, current_q: ArrayLike, key: jnp.ndarray) -> Tuple[ArrayLike, float]:
+        pass
+
+    def run_hmc(self, U: Callable[[ArrayLike], float], grad_U: Callable[[ArrayLike], ArrayLike], epsilon: float, L: int, initial_q: ArrayLike, num_samples: int, key: jnp.ndarray) -> ArrayLike:
+        pass
+
+
