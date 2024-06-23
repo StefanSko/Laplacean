cb6fc4bf2ac23b79a73eadba92db7f1296590bb5
SkoruS01 <Stefan.sko@gmail.com>
2024-06-23 17:02:12 +0200

    Refactor HMC Protocol wrt typing

--- laplacean/backend/base.py
+++ laplacean/backend/base.py
@@ -1,4 +1,4 @@
-from typing import Protocol, Callable, Tuple, Union, Float
+from typing import Protocol, Callable, Union
 import numpy as np
 import jax.numpy as jnp
 
@@ -6,15 +6,15 @@ ArrayLike = Union[np.ndarray, jnp.ndarray]
 
 
 # Define type annotations for clarity
-PotentialFn = Callable[[ArrayLike], Float]
+PotentialFn = Callable[[ArrayLike], float]
 GradientFn = Callable[[ArrayLike], ArrayLike]
 
 class HMC(Protocol):
 
-    def hmc(self, U: PotentialFn, grad_U: GradientFn, epsilon: Float, L: int, current_q: ArrayLike) -> Tuple[ArrayLike, Float]:
+    def hmc(self, U: PotentialFn, grad_U: GradientFn, epsilon: float, L: int, current_q: ArrayLike) -> ArrayLike:
         pass
 
-    def run_hmc(self, U: PotentialFn, grad_U: GradientFn, epsilon: Float, L: int, initial_q: ArrayLike, num_samples: int) -> ArrayLike:
+    def run_hmc(self, U: PotentialFn, grad_U: GradientFn, epsilon: float, L: int, initial_q: ArrayLike, num_samples: int) -> ArrayLike:
         pass
 
 
