79ff78e15c976090581443503260e4d211ffa836
StefanSko <Stefan.sko@gmail.com>
2024-07-27 15:18:15 +0200

    cleanup base jax hmc class from tests

--- laplacean/backend/jax_hmc.py
+++ laplacean/backend/jax_hmc.py
@@ -7,9 +7,6 @@ import jax_dataclasses as jdc
 
 import jax.random as random
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 # Define type annotations for clarity
 PotentialFn = Callable[[Array], float]
 GradientFn = Callable[[Array], Array]
@@ -82,36 +79,3 @@ class JaxHMC(HMCProtocol):
             return (output, output.key), output.current_q
         _, samples = jax.lax.scan(body_fun, (input, input.key), jnp.zeros(num_samples))
         return samples
-
-
-#
-# Example usage for potential energy function corresponding to a standard normal distribution with mean 0 and variance 1
-# U needs to return the negative log of the density
-@jax.jit
-def U(q: Array) -> Array:
-    return 0.5 * jnp.sum(q ** 2)
-
-@jax.jit
-def grad_U(q: Array) -> Array:
-    return jax.grad(U)(q)
-
-initial_q = jnp.array([1.])
-
-hmc: HMCProtocol = JaxHMC(U=U, grad_U=grad_U)
-input: JaxHMCData = JaxHMCData(epsilon=0.1, L=10, current_q=initial_q, key=random.PRNGKey(0))
-samples = hmc.run_hmc(input, 1000)
-
-print(jnp.mean(samples)) 
-print(jnp.var(samples))
-
-
-sns.kdeplot(samples)
-plt.xlabel('Sample Value')
-plt.title('Distribution of Samples')
-plt.show()
-
-sns.lineplot(samples)
-plt.xlabel('Step')
-plt.ylabel('Sample Value')
-plt.title('Trace of Samples')
-plt.show()
